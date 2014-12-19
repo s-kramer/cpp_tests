7f4c1d7a2ddf4bdc0592011dd20e5546bc030d2a
Sebastian Kramer <sebastian.kramer@wp.pl>
2014-12-19 16:49:35 +0100

    PSI system call tests + arg deduction

---
+++ t95_class_template_arg_deduction.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <boost/function.hpp>
+
+template <typename T>
+class Foo
+{
+public:
+    explicit Foo (boost::function<T(void)> func) : func_(func) {};
+private:
+    boost::function<T(void)> func_;
+};
+
+template <typename T>
+class Bar
+{
+public:
+    explicit Bar (T bar) : bar_(bar) {};
+private:
+    T bar_;
+};
+
+int main()
+{
+    // Foo f;
+    Bar b;
+    return 0;
+}
